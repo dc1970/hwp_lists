3714fd3d9de534dd78d2e899611a9a55340089a8
dc1970 <doron.caspi@gmail.com>
2020-10-05 22:19:35 -0400

    Remove clocks

--- hwp_lists.py
+++ hwp_lists.py
@@ -16,11 +16,13 @@ def create_ver_dictinary(dict, file_name):
         dict[line_l[0]] = line_l[3:]
 
 if __name__ == '__main__':
-    outfile = open('dif_hwp_results', 'w')
+    outfile = open('diff_hwp_results.csv', 'w')
     create_ver_dictinary(old_hwp_dict, old_ver)
     create_ver_dictinary(new_hwp_dict, new_ver)
 
     for k in new_hwp_dict.keys():
+        if 'CLK' in k:
+            continue  # Remove clocks
         if k in old_hwp_dict:
             if old_hwp_dict[k] != new_hwp_dict[k]:
                 diff_hwp_dict[k] = [' '.join(old_hwp_dict[k]), ' '.join(new_hwp_dict[k])]
@@ -28,6 +30,6 @@ if __name__ == '__main__':
     sorted_keys = list(diff_hwp_dict.keys())
     sorted_keys.sort()
     for k in sorted_keys:
-        outfile.write('{}, {}\n'.format(k, ', '.join(diff_hwp_dict[k])))
+        outfile.write('{},\t {}\n'.format(k, ',\t'.join(diff_hwp_dict[k])))
 
 
